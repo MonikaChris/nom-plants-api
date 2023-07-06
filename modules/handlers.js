51051b35a55c4774c4006b9df031ad344bc72dfd
Monika <monidavies@gmail.com>
2023-07-06 16:35:31 -0700

    Updates addPlants handler to add plant to existing entry or create new entry

--- modules/handlers.js
+++ modules/handlers.js
@@ -14,16 +14,39 @@ Handler.getWeeks = async (req, res, next) => {
   }
 };
 
+// Handler.addPlant = async (req, res, next) => {
+//   try {
+//     const { date, plants, email } = req.body;
+//     const newWeek = new Week({ date, plants, email });
+//     const week = await newWeek.save();
+//     res.status(200).send(week);
+//   } catch (error) {
+//     console.error(error);
+//     next(error);
+//   }
+// };
+
 Handler.addPlant = async (req, res, next) => {
-  try {
-    console.log(req.body);
-    const { date, plants, email } = req.body;
-    const newWeek = new Week({ date, plants, email });
-    const week = await newWeek.save();
-    res.status(200).send(week);
-  } catch (error) {
-    console.error(error);
-    next(error);
+  const week = await Week.findOne({date: req.body.date});
+
+  //If week exists update with new plant, else create new week with first plant
+  if (week) {
+    week.plants.push(req.body.plants);
+    try {
+      const updatedWeek = await Week.findByIdAndUpdate(week.id, week, {new: true});
+      res.status(200).send(updatedWeek);
+    } catch(error) {
+      console.error(error);
+      next(error);
+    }
+  } else {
+    try {
+      const newWeek = await Week.create(req.body);
+      res.status(200).send(newWeek);
+    } catch(error) {
+      console.error(error);
+      next(error);
+    }
   }
 };
 
