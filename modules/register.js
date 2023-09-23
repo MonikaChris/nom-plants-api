65d5b01865828f103ba2292692ca43617eb1f14d
Monika <monidavies@gmail.com>
2023-09-22 18:29:07 -0700

    Adds return statements

--- modules/register.js
+++ modules/register.js
@@ -4,10 +4,10 @@ const bcrypt = require('bcrypt');
 async function registerUser(req, res, next) {
   const { username, password } = req.body;
 
-  if (!username || !password) res.status(400).send('Username and password required');
+  if (!username || !password) return res.status(400).send('Username and password required');
 
   const user = await User.findOne({ username });
-  if (user) res.status(409).send('Username taken');
+  if (user) return res.status(409).send('Username taken');
 
   try {
     const hashedPwd = await bcrypt.hash(password, 10);
@@ -17,7 +17,7 @@ async function registerUser(req, res, next) {
       password: hashedPwd
     });
 
-    res.status(201).send('Registered new user');
+    return res.status(201).send('Registered new user');
 
   } catch(error) {
     next(error);
