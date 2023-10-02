8e57e9dbbdadb74240c34d5d0150e0873622f256
Monika <monidavies@gmail.com>
2023-10-02 07:36:07 -0700

    Changes email to username

--- models/week.js
+++ models/week.js
@@ -1,14 +1,14 @@
-'use strict';
+"use strict";
 
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const weekSchema = new Schema({
   date: String, //"MM-DD-YYYY"
   plants: [String],
-  email: String
+  username: String,
 });
 
-const Week = mongoose.model('Week', weekSchema);
+const Week = mongoose.model("Week", weekSchema);
 
 module.exports = Week;
